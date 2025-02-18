36b14f11d2a4c0e295545e2c6295933d3fa89343
atebha <ahsantebha@outlook.com>
2025-02-18 14:02:40 -0800

    Force function and inputs

--- Main.py
+++ Main.py
@@ -1,6 +1,10 @@
-# Step 1
+# Inputs for functions
+train_mass = 22680
+train_acceleration = 10
+train_distance = 100
+bomb_mass = 1
 
-# Step 2
+# Step 1 & 2
 # Fahrenhit to Celsius conversion function
 def f_to_c(f_temp):
     return (f_temp - 32) * (5 / 9)
@@ -9,7 +13,7 @@ def f_to_c(f_temp):
 f100_in_celcius = f_to_c(100)
 print(f100_in_celcius)
 
-# Step 3
+# Step 3 & 4 
 def c_to_f(c_temp):
     f_temp = c_temp * (9 / 5) + 32
     return f_temp
@@ -17,3 +21,11 @@ def c_to_f(c_temp):
 # Testing c_to_f function
 c100_in_fahrenheit = c_to_f(100)
 print(c100_in_fahrenheit)
+
+# Step 5 & 6
+def get_force(mass, acceleration):
+    return mass * acceleration
+
+# Save force value to variable
+train_force = get_force(train_mass, train_acceleration)
+print(train_force)
