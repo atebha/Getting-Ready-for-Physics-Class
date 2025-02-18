321a8f1f1a53e0768377b38dae4e8cd7eae5cb1c
atebha <ahsantebha@outlook.com>
2025-02-18 13:26:08 -0800

    Create and test c to f function

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 # Step 1
 
+# Step 2
 # Fahrenhit to Celsius conversion function
 def f_to_c(f_temp):
     return (f_temp - 32) * (5 / 9)
@@ -7,3 +8,12 @@ def f_to_c(f_temp):
 # Testing f_to_c function
 f100_in_celcius = f_to_c(100)
 print(f100_in_celcius)
+
+# Step 3
+def c_to_f(c_temp):
+    f_temp = c_temp * (9 / 5) + 32
+    return f_temp
+
+# Testing c_to_f function
+c100_in_fahrenheit = c_to_f(100)
+print(c100_in_fahrenheit)
